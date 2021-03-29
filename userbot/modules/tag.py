739eb51c5ec1074ff523fa8fd3550aafc7be9ed1
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-03-29 15:32:05 +0700

    Create tag.py

---
+++ userbot/modules/tag.py
@@ -0,0 +1,82 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# Recode by @mrismanaziz
+# @sharinguserbot
+
+
+from telethon.tl.types import ChannelParticipantAdmin as admin
+from telethon.tl.types import ChannelParticipantCreator as owner
+from telethon.tl.types import UserStatusOffline as off
+from telethon.tl.types import UserStatusOnline as onn
+from telethon.tl.types import UserStatusRecently as rec
+from telethon.utils import get_display_name
+
+from userbot.events import register
+from userbot import CMD_HELP, bot
+
+
+@register(outgoing=True, pattern=".tag(on|off|all|bots|rec|admins|owner)?(.*)", disable_errors=True)
+async def _(e):
+    okk = e.text
+    lll = e.pattern_match.group(2)
+    users = 0
+    o = 0
+    nn = 0
+    rece = 0
+    if lll:
+        xx = f"{lll}"
+    else:
+        xx = ""
+    async for bb in e.client.iter_participants(e.chat_id, 99):
+        users = users + 1
+        x = bb.status
+        y = bb.participant
+        if isinstance(x, onn):
+            o = o + 1
+            if "on" in okk:
+                xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if isinstance(x, off):
+            nn = nn + 1
+            if "off" in okk:
+                if not (bb.bot or bb.deleted):
+                    xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if isinstance(x, rec):
+            rece = rece + 1
+            if "rec" in okk:
+                if not (bb.bot or bb.deleted):
+                    xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if isinstance(y, owner):
+            if "admin" or "owner" in okk:
+                xx += f"\n꧁[{get_display_name(bb)}](tg://user?id={bb.id})꧂"
+        if isinstance(y, admin):
+            if "admin" in okk:
+                if not bb.deleted:
+                    xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if "all" in okk:
+            if not (bb.bot or bb.deleted):
+                xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if "bot" in okk:
+            if bb.bot:
+                xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+    await e.client.send_message(e.chat_id, xx)
+    await e.delete()
+
+
+CMD_HELP.update({
+    "tagger":
+    "`.tagall`\
+    \nUsage: Tag Top 100 Members of chat.\
+    \n\n`.tagadmins`\
+    \nMenampilkan: Tag Admins of that chat.\
+    \n\n`.tagowner`\
+    \nMenampilkan: Tag Owner of that chat\
+    \n\n`.tagbots`\
+    \nMenampilkan: Tag Bots of that chat.\
+    \n\n`.tagrec`\
+    \nMenampilkan: Tag recently Active Members.\
+    \n\n`.tagon`\
+    \nMenampilkan: Tag online Members(work only if privacy off)\
+    \n\n`.tagoff`\
+    \nMenampilkan: Tag Offline Members(work only if privacy off)"
+})
