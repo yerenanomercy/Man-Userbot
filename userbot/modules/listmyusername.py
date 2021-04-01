d4b1a366d147de0f4814d6040337ba650b88439e
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-04-01 13:46:38 +0700

    Create listmyusername.py

---
+++ userbot/modules/listmyusername.py
@@ -0,0 +1,26 @@
+# Recode by @mrismanaziz
+# @sharinguserbot
+
+from telethon import events, functions, types
+from userbot.events import register
+from userbot import CMD_HELP
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
+
+
+@register(outgoing=True, pattern=".listmyusername ?(.*)")
+async def mine(event):
+    """ For .reserved command, get a list of your reserved usernames. """
+    result = await bot(GetAdminedPublicChannelsRequest())
+    output_str = ""
+    for channel_obj in result.chats:
+        output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
+    await event.edit(output_str)
+
+
+CMD_HELP.update(
+    {
+        "LIST MY USERNAMES": "**Plugin : **`listmyusernames`\
+    \n\n**Syntax : **`.listmyusername`\
+    \n**Function : **this plugin give you your all channel and groups usernamen"
+    }
+)
